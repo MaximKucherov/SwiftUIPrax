6a4350a2aacddcfe5efff337990f47d92e084273
Maxim Kucherov <maxim.kucherov.v@gmail.com>
2024-02-28 21:36:32 +0100

    Color Rectangle
    Done

--- SwiftUIPrax/Lesson_2/MyHW_2/LessonHW_2.swift
+++ SwiftUIPrax/Lesson_2/MyHW_2/LessonHW_2.swift
@@ -9,17 +9,41 @@ import SwiftUI
 
 struct Lesson2_HW: View {
 
+    @State private var redSliderValue: Double = 0
+    @State private var greenSliderValue: Double = 0
+    @State private var blueSliderValue: Double = 0
+    @State private var stringValue: String = ""
+
     var body: some View {
         ZStack {
-            Color(.blue).ignoresSafeArea()
+            Color(.blue.withAlphaComponent(0.8)).ignoresSafeArea()
             VStack {
                 RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                    .foregroundStyle(.green)
+                    .foregroundStyle(Color(red: redSliderValue/255, green: greenSliderValue/255, blue: blueSliderValue/255))
                     .frame(CGSize(width: 350, height: 150))
                     .overlay(RoundedRectangle(cornerRadius: 25.0).stroke(Color.white, lineWidth: 4))
+                    .padding(.bottom, 40)
+                VStack {
+                    HStack {
+                        TextValue(value: $redSliderValue)
+                        SliderColor(value: $redSliderValue).tint(.red)
+                        TextFieldValue(value: $redSliderValue, textField: $stringValue)
+                    }
+                    HStack {
+                        TextValue(value: $greenSliderValue)
+                        SliderColor(value: $greenSliderValue).tint(.green)
+                        TextFieldValue(value: $greenSliderValue, textField: $stringValue)
+                    }
+                    HStack {
+                        TextValue(value: $blueSliderValue)
+                        SliderColor(value: $blueSliderValue).tint(.blue)
+                        TextFieldValue(value: $blueSliderValue, textField: $stringValue)
+                    }
+                }
+                .padding(.horizontal, 10)
                 Spacer()
-                
             }
+
         }
     }
 }
--- SwiftUIPrax/Lesson_2/MyHW_2/SliderHW_2.swift
+++ SwiftUIPrax/Lesson_2/MyHW_2/SliderHW_2.swift
@@ -14,31 +14,10 @@ struct SliderHW_2: View {
     @State private var textFieldValue: String = ""
 
     var body: some View {
-        ZStack {
-            Color.blue.ignoresSafeArea()
-            HStack {
-                Text("\(lround(sliderValue))")
-                    .foregroundStyle(Color.white)
-                    .frame(CGSize(width: 35, height: 45))
-
-                SliderColor(value: $sliderValue, tintColor: .red)
-                
-                ZStack(alignment: .trailing) {
-                    TextField("", text: $textFieldValue)
-                        .padding(.horizontal, 8)
-                        .frame(width: 75, height: 45)
-                        .multilineTextAlignment(.trailing)
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .keyboardType(.default)
-                    if textFieldValue.isEmpty {
-                        Text("\(lround(sliderValue))")
-                            .padding(.horizontal, 8)
-                    }
-
-                }
-            }
-            .padding()
+        HStack {
+            TextValue(value: $sliderValue)
+            SliderColor(value: $sliderValue)
+            TextFieldValue(value: $sliderValue, textField: $textFieldValue)
         }
     }
 }
@@ -48,12 +27,37 @@ struct SliderHW_2: View {
 }
 
 struct SliderColor: View {
-
     @Binding var value: Double
-    var tintColor: Color
-
     var body: some View {
-        Slider(value: $value, in: 0...255).tint(tintColor)
+        Slider(value: $value, in: 0...255)
+    }
+}
 
+struct TextValue: View {
+    @Binding var value: Double
+    var body: some View {
+        Text("\(lround(value))")
+            .foregroundStyle(Color.white)
+            .frame(CGSize(width: 35, height: 45))
+    }
+}
+
+struct TextFieldValue: View {
+    @Binding var value: Double
+    @Binding var textField: String
+    var body: some View {
+        ZStack(alignment: .trailing) {
+            TextField("", text: $textField)
+                .padding(.horizontal, 8)
+                .frame(width: 60, height: 40)
+                .multilineTextAlignment(.trailing)
+                .background(Color.white)
+                .cornerRadius(10)
+                .keyboardType(.default)
+            if textField.isEmpty {
+                Text("\(lround(value))")
+                    .padding(.horizontal, 8)
+            }
+        }
     }
 }
